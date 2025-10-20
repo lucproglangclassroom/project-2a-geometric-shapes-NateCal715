6d140780e1804608be40e06ae9b013fcbd6dae8b
NateCal715 <157073389+NateCal715@users.noreply.github.com>
2025-10-20 10:55:39 -0500

    Updates to shapes.scala

--- src/main/scala/shapes.scala
+++ src/main/scala/shapes.scala
@@ -6,24 +6,5 @@ enum Shape derives CanEqual:
   case Location(x: Int, y: Int, shape: Shape)
   case Ellipse(rx: Int, ry: Int)
   case Group(shapes: Shape*)
-
-  //  val simpleGroup = Group(
-  //    Location(200, 100, Ellipse(50, 30)),
-  //    Location(400, 300, Rectangle(100, 50))
-  //  )
-  //  val complexGroup =
-  //    Location(50, 100,
-  //      Group(
-  //        Ellipse(20, 40),
-  //        Location(150, 50,
-  //        Group(
-  //          Rectangle(50, 30),
-  //          Rectangle(300, 60),
-  //          Location(100, 200,
-  //            Ellipse(50, 30)
-  //          )
-  //      )),
-  //      Rectangle(100, 200)
-  //    ))
   
   // TODO add missing cases (see test fixtures)
